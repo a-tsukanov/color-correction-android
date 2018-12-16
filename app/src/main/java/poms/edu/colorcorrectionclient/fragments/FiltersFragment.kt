36b6a2ed49243bb8e138ab4f8be2a028c7d09c0d
sasha <sasha.ts91@gmail.com>
2018-12-16 14:22:16 +0300

    copyright notice added to code

--- app/src/main/java/poms/edu/colorcorrectionclient/fragments/FiltersFragment.kt
+++ app/src/main/java/poms/edu/colorcorrectionclient/fragments/FiltersFragment.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018 Aliaksandr Tsukanau.
+ * Licensed under MIT Licence.
+ * You may not use this file except in compliance with MIT License.
+ * See the MIT License for more details. https://www.mitlicense.org/
+ */
+
 package poms.edu.colorcorrectionclient.fragments
 
 import android.content.Context
